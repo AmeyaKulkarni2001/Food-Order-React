a982d22cd844f64b3cc38c7cdaceb27952976193
A K <ameya.2000k@gmail.com>
2021-12-07 17:12:14 +0530

    Getting data from db

--- src/componenets/Meals/AvailableMeals.js
+++ src/componenets/Meals/AvailableMeals.js
@@ -1,10 +1,39 @@
-import DUMMY_MEALS from "./Dummy-Meals";
+
+import { useEffect, useState } from "react";
+
+
+// import DUMMY_MEALS from "./Dummy-Meals";
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async() => {
+      console.log(process.env.LINK)
+      const res = await fetch('https://movie-f5957-default-rtdb.firebaseio.com/meals.json');
+      const resData = await res.json();
+
+      const loadedMeals = [];
+
+      for(const key in resData){
+        loadedMeals.push({
+          id: key,
+          name: resData[key].name,
+          description: resData[key].description,
+          price: resData[key].price
+        });
+      }
+      setMeals(loadedMeals);
+
+    };
+    fetchMeals();
+    
+  },[])
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
